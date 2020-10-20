2be922dec2420d0dd28b4e3d1aa5e2a52e6fc6c5
Mano <rtorbed@roc-dev.com>
2020-10-20 16:32:19 +0200

    h05 klaar

---
+++ h06/Opdracht1.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Opdrachten</title>
+    <style>
+        table, tr, td {
+            border: solid 1px black;
+            border-collapse: collapse;
+        }
+        td {
+            padding: 20px;
+        }
+    </style>
+</head>
+<body>
+
+<table>
+
+<?php
+
+
+$server = "localhost";
+$dbname = "phpschool";
+$username = "root";
+$password = "";
+
+
+
+
+try {
+    $conn = new PDO("mysql:host={$server};port=3308;dbname={$dbname}", $username, $password);
+    echo "Connected successfully";
+} catch (PDOException $error) {
+    echo "Connection failed: " . $error->getMessage();
+}
+$query = "SELECT * FROM cursist";
+$stmt = $conn->prepare($query) or die ("Error 1.");
+$stmt->execute() or die("Error 2.");
+
+while ($array = $stmt->fetch()) {
+    echo "<tr>";
+    echo "<td>" . $array['cursistnr'] . "</td>";
+    echo "<td>" . $array['naam'] . "</td>";
+    echo "<td>" . $array['roepnaam'] . "</td>";
+    echo "<td>" . $array['straat'] . "</td>";
+    echo "<td>" . $array['postcode'] . "</td>";
+    echo "<td>" . $array['plaats'] . "</td>";
+    echo "<td>" . $array['geslacht'] . "</td>";
+    echo "<td>" . $array['geb_datum'] . "</td>";
+    echo "</tr>";
+}
+?>
+
+</table>
+
+</body>
+</html>
